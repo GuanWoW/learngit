c8e6616c3c197e801592177046a52be1c285ed9e
ymin <ymim_guan@163.com>
2021-08-05 14:35:14 +0800

    mark uuid

---
+++ mark.sh
@@ -0,0 +1,11 @@
+
+
+
+cd SLT/ects-1.1.30-x86_64-linux-rel-b38fb8122
+
+echo -n "enter Mark ID:"
+read ID
+echo "Mark ID=$ID" | tee -a uuid_mark.txt
+
+uuid=`./efsmt -otp uuid | grep "UUID"`
+echo "$uuid" | tee -a uuid_mark.txt
